c3d5dba8fc57fc47a995fb180b777001f496a564
André Matos <andrermatos90@gmail.com>
2024-04-09 10:32:07 -0300

    reiniciando o curso

--- Hora de Codar/projeto/src/App.js
+++ Hora de Codar/projeto/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Challenge02 from './components/Challenge02';
+import logo from "./logo.svg";
+import "./App.css";
+import Challenge02 from "./components/Challenge02";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Challenge02></Challenge02>
-       
       </header>
+
+      <div className="desafios">
+        <h1>Desafios</h1>
+        <ol>
+          <li>Desafio 1</li>
+        </ol>
+      </div>
     </div>
   );
 }
-
-
 
 export default App;
