13716cc50889c2cb5af6da66435135538ac08a46
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-05-09 20:20:16 -0300

    warning ao renderizar lista

--- Hora de Codar/02 - AvançandoEmReact/projeto/src/components/ListRender.jsx
+++ Hora de Codar/02 - AvançandoEmReact/projeto/src/components/ListRender.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ListRender = () => {
+  const [list] = useState(["Gol", "Civic", "Malibu", "Twingo", "Fusca"]);
 
-    const [list] = useState(["Gol", "Civic", "Malibu", "Twingo", "Fusca"]);
+  const [users] = useState([
+    { id: 1, name: "andré", age: 34 },
+    { id: 3, name: "lordao", age: 40 },
+    { id: 5, name: "lauro antigo", age: 51 },
+  ]);
+
   return (
     <div>
-        <ul>
-            {list.map((item)=>(
-                <li>{item}</li>
-            ))}
-        </ul>
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ListRender
+export default ListRender;
