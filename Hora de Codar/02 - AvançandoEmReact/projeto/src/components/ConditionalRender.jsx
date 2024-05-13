2d445345ba3b147c4c817aa5f2fdd7962b437ab8
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-05-13 14:25:01 -0300

    if e else no react

--- Hora de Codar/02 - AvançandoEmReact/projeto/src/components/ConditionalRender.jsx
+++ Hora de Codar/02 - AvançandoEmReact/projeto/src/components/ConditionalRender.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react"
 
 const ConditionalRender = () => {
-    const [x] = useState(false)
+    const [x] = useState(false);
+    const [name] = useState("Jubileu");
 
     return(
         <>
+
+       {/*condicional simples*/}
+
        {x && <p>x é verdadeiro</p>}
        {!x && <p>agora x é falso</p>}
+
+
+       {/*if else ternario*/}
+
+       {name === "Jubileu" ? (<p>O nome é Jubileu</p> ) : (  <p>O nome não é Jubileu</p> ) }
         
         </>
     )
