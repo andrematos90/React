dae7dda02b7e0d23ae9ce5a1c6dd2a79615b953c
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-05-14 16:32:18 -0300

    CSS de componente

---
+++ Hora de Codar/03 - React e CSS/projeto/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../components/MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <p className='paragrafo'>meu componente</p>
+    </div>
+  )
+}
+
+export default MyComponent;
