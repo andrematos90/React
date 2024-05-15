1c911fc6042e80f0322bedb28f8520d7159373f4
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-05-15 14:13:25 -0300

    label envolvendo input

--- Hora de Codar/04 - Formulários com React/projeto/src/components/Form.jsx
+++ Hora de Codar/04 - Formulários com React/projeto/src/components/Form.jsx
@@ -7,8 +7,15 @@ const Form = () => {
     {/*criação de form */}
     <form>
         <div>
+          {/*01 - Criando formulário */}
             <label htmlFor="name">Nome:</label>
             <input type="text" name='name' placeholder='Digite seu nome'/>
+
+            {/*02 - label envolvendo input */}
+            <label>
+              <span>E-mail:</span>
+              <input type="email" name='email' placeholder='Digite seu e-mail'/>
+            </label>
         </div>
         <input type="submit" value="Enviar"/>
     </form>
