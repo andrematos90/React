8e34680a8748b84db3e706274a66d0e233b60423
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-05-24 14:31:34 -0300

    navegação entre páginas

--- Hora de Codar/07 - React Router/projeto/src/App.js
+++ Hora de Codar/07 - React Router/projeto/src/App.js
@@ -2,18 +2,21 @@
 import './App.css';
 
 // pacotes do React Router
-import { BrowserRouter, Routes, Route } from 'react-root-dom'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //pages
 
 import Home from './pages/Home'
 import About from './pages/About'
+
+//components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="about" element={<About/>}/>
