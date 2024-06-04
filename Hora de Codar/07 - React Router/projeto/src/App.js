7c4d7466ffbaa05e203b128555e2129cb9407cc5
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-06-04 15:45:55 -0300

    carregando dados

--- Hora de Codar/07 - React Router/projeto/src/App.js
+++ Hora de Codar/07 - React Router/projeto/src/App.js
@@ -1,17 +1,12 @@
-
-import './App.css';
-
+import "./App.css";
 // pacotes do React Router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //pages
-
-import Home from './pages/Home';
-import About from './pages/About';
-import Product from './pages/Product';
-
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
 //components
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -19,17 +14,15 @@ function App() {
       <h1>React Router</h1>
       {/*02 links com react router */}
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
 
-        {/*rota dinamica */}
-        <Route path='products/:id' element={<Product/>}/>
-
-      </Routes>
+          {/*rota dinamica */}
+          <Route path="products/:id" element={<Product />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
