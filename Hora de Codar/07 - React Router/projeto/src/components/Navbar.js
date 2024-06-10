ee8b15a29283e5c8147a7ea4b8febdadae0ce7a7
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-06-10 13:43:22 -0300

    link ativo

--- Hora de Codar/07 - React Router/projeto/src/components/Navbar.js
+++ Hora de Codar/07 - React Router/projeto/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink } from 'react-router-dom';
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav>
       
-        <Link to="/ ">Home</Link>
-        <Link to="/about">Sobre</Link>
+        {/*<Link to="/ ">Home</Link>
+        <Link to="/about">Sobre</Link>*/}
+
+        <NavLink to="/" className={({isActive}) =>(isActive ? "ativo" : "não-ativo")}>Home</NavLink>
+        <NavLink to="about" className={({isActive}) =>(isActive ? "ativo" : "não-ativo")}>Sobre</NavLink>
         
     </nav>
   )
