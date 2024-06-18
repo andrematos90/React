bdb6bfe71d57ac828fb586db0e6a56296b6b2c84
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-06-18 10:18:51 -0300

    CSS de Footer e Navbar

--- Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Footer/Footer.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './../Footer/Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Escreva sobre o que você gosta!</h3>
       <p>miniBlog &copy; 2024</p>
     </footer>
--- Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Navbar/Navbar.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import { NavLink } from "react-router-dom";
+import styles from './../Navbar/Navbar.module.css'
 
 
 const Navbar = () => {
   return (
-    <nav>
-      <NavLink to="/">
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
         Mini <span>Blog</span>
       </NavLink>
 
-      <ul>
+      <ul className={styles.links_list}> 
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" className={({isActive}) => (isActive? styles.active : "")}>Home</NavLink>
         </li>
         <li>
-          <NavLink to="About">Sobre</NavLink>
+          <NavLink to="About" className={({isActive}) => (isActive? styles.active : "")}>Sobre</NavLink>
         </li>
       </ul>
     </nav>
