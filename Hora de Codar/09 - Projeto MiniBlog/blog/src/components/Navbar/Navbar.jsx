87367511f6b89b2a77cd29cfc3d4fa4fdacd9fe6
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-06-21 16:20:22 -0300

    ..

--- Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Navbar/Navbar.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/blog/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styles from './../Navbar/Navbar.module.css'
 
+import { useAuthentication } from '../../hooks/useAuthentication'
+import { useAuthValue } from '../../context/AuthContext'
+
 
 const Navbar = () => {
+
+  const { user } = useAuthValue();
+  
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.brand}>
@@ -13,12 +19,17 @@ const Navbar = () => {
         <li>
           <NavLink to="/" className={({isActive}) => (isActive? styles.active : "")}>Home</NavLink>
         </li>
-        <li>
+        {!user && (
+          <>
+            <li>
           <NavLink to="/Login" className={({isActive}) => (isActive? styles.active : "")}>Login</NavLink>
         </li>
         <li>
           <NavLink to="/Register" className={({isActive}) => (isActive? styles.active : "")}>Register</NavLink>
         </li>
+          </>
+        )}
+      
         <li>
           <NavLink to="/About" className={({isActive}) => (isActive? styles.active : "")}>Sobre</NavLink>
         </li>
