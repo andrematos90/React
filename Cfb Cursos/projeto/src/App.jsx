dcc965a873c995110ef6d2c9821024c4b02b6b05
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-06-26 16:31:19 -0300

    funçoes por props

--- Cfb Cursos/projeto/src/App.jsx
+++ Cfb Cursos/projeto/src/App.jsx
@@ -1,21 +1,39 @@
-import './App.css'
-import Home from './pages/home/Home'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import "./App.css";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const rocket = "Falcon 9";
+  const company = "SpaceX";
 
-  const rocket = 'Falcon 9';
-  const company = 'SpaceX';
- 
+  const imprimeNumero = () => {
+    return "numero 9";
+  };
+
+
+  const somar = (v1, v2) =>{
+    return v1 + v2
+  }
 
   return (
     <>
-    <Header/>
-    <Home msg={"Olá React!"} nome={"Binzão"} rocket ={rocket} empresa = {company}/>
-    <Footer/>
+      <Header />
+
+      <Home
+
+        //atributos que serão enviados ao componente por props
+        msg={"Olá React!"}
+        nome={"Binzão"}
+        rocket={rocket}
+        empresa={company}
+        fnc={imprimeNumero}
+        somar={somar}
+      />
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
