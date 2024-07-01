c4948badd2553dc47c1f14b2930b0373264c51ae
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-07-01 15:50:47 -0300

    renderização condicional

--- Cfb Cursos/projeto1/src/App.jsx
+++ Cfb Cursos/projeto1/src/App.jsx
@@ -5,9 +5,46 @@ import "./App.css";
 function App() {
   const [ligado, setLigado] = useState(false);
 
+  const BLUE = "blue";
+  const GREEN = "green";
+  const YELLOW = "yellow";
+
+  const colorStyles = {
+    blue: { color: "#66CDAA" },
+    green: { color: "#66CDAA" },
+    yellow: { color: "#FFFF00" },
+  };
+
+  const [cor, setCor] = useState(BLUE);
+
+  const changeColor = (cor) => {
+    switch (cor) {
+      case BLUE:
+        setCor(GREEN);
+        break;
+      case GREEN:
+        setCor(YELLOW);
+        break;
+      case YELLOW:
+        setCor(BLUE);
+        break;
+      default:
+        setCor(BLUE);
+    }
+  };
+
   return (
     <>
       <Led ligado={ligado} setLigado={setLigado} />
+
+      <h1 style={colorStyles[cor]}>ooooooooooooooi</h1>
+      <button
+        onClick={() => {
+          changeColor(cor);
+        }}
+      >
+        troca
+      </button>
     </>
   );
 }
