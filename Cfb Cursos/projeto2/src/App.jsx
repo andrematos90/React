aa9335d19fb51e063d89d77b406e7094c5289201
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-07-03 09:25:04 -0300

    praticando

--- Cfb Cursos/projeto2/src/App.jsx
+++ Cfb Cursos/projeto2/src/App.jsx
@@ -3,18 +3,45 @@ import { useState } from 'react'
 
 function App() {
  
+
+  {/*uso do método map */}
   const carros = ["Twingo", "Fusca", "Malibu", "Civic", "Gol"];
 
+
+   {/*manipulando formulários */}
+   const [nome, setNome] = useState();
+
+
+   const handleChangeName = (e) =>{
+    setNome(e.target.value);
+
+   }
 
 
   return (
     <>
+
+    {/*uso do método map */}
     <ul>{carros.map((carro, index) =>(
-      <li>{carro}</li>
+      <li >{carro}</li>
     ))}</ul>
+
+    <br />
+
+   {/*manipulando formulários */}
+    <form>
+      <label>Digite seu nome</label>
+      <input type="text" name='nome' value={nome}  onChange={(e)=>handleChangeName(e)}/>
+     
+    </form>
+
+
+    <h1>Nome:</h1> <p>{nome}</p>
+
       
     </>
   )
 }
+
 
 export default App
