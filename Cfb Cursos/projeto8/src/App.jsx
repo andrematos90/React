45662223e1c84180a7eee3fbe7c7cd8e0a9d86b3
André Matos <103394184+andrematos90@users.noreply.github.com>
2024-07-29 10:46:58 -0300

    navegação entre paginas com react nativo

--- Cfb Cursos/projeto8/src/App.jsx
+++ Cfb Cursos/projeto8/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [page, setPage] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const url = window.location.href;
+    const res = url.split("?");
+    setPage(res[1]);
+  });
+
+
+  const LinksPaginas=(p)=>{
+    if(p==1){
+      window.open('https://localhost:5173?1','_self')
+    }else if(p==2){
+      window.open('https://localhost:5173/2','_self')
+    }else if(p==3){
+      window.open('https://localhost:5173?3', '_self')
+    }else{
+      window.open('https://localhost:5173?0', '_self')
+    }
+  }
+
+  const retornarPagina = () => {
+    if (page == 1) {
+      return <Page1 />;
+    } else if (page == 2) {
+      return <Page2 />;
+    } else if (page == 3) {
+      return <Page3 />;
+    } else {
+      return (
+        <div>
+          <button onClick={()=>{LinksPaginas(1)}}>Page 1</button>
+          <button onClick={()=>{LinksPaginas(2)}}>Page 2</button>
+          <button onClick={()=>{LinksPaginas(3)}}>Page 3</button>
+          <button onClick={()=>{LinksPaginas(0)}}>Page 0</button>
+         
+        </div>
+      );
+    }
+  };
+
+  return <>{retornarPagina()}</>;
 }
 
-export default App
+export default App;
+
+/*
+
+
+OUTRA FORMA
+
+import "./App.css";
+import Page1 from "./components/Page1";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [page, setPage] = useState(0);
+
+  const retornarPagina = () => {
+    if (page == 1) {
+      return <Page1 />;
+    } else if (page == 2) {
+      return <Page2 />;
+    } else if (page == 3) {
+      return <Page3 />;
+    } else {
+      return (
+        <div>
+          <button onClick={()=>{setPage(1)}}>Page 1</button>
+          <button onClick={()=>{setPage(2)}}>Page 2</button>
+          <button onClick={()=>{setPage(3)}}>Page 3</button>
+          <button onClick={()=>{setPage(0)}}>Home</button>
+        </div>
+      );
+    }
+  };
+
+  return <>{retornarPagina()}</>;
+}
+
+export default App;
+
+
+
+*/
