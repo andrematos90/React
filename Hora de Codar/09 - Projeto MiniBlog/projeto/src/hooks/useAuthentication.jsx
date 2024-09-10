c2446ccaa707112c140a4aa6f437f374a6fcb1e4
André Matos <andrerosadematos90@gmail.com>
2024-09-10 20:11:09 -0300

    criando hook de autentificação

--- Hora de Codar/09 - Projeto MiniBlog/projeto/src/hooks/useAuthentication.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/projeto/src/hooks/useAuthentication.jsx
@@ -13,6 +13,19 @@ const useAuthentication = () => {
  const [error,setError]=useState(null);
  const [loading,setLoading]=useState(null);
 
+ //cleanup
+ //deal with memory leak
+ const[cancelled, setCancelled]=useState(false);
+
+ const auth = getAuth();
+
+
+ function checkIfCancelled(){
+    if(cancelled){
+        return;
+    }
+ }
+
 }
 
 export default useAuthentication;
