d3938ed8e3975fc85ddcf69576eeb01ee8db7d0b
André Matos <andrerosadematos90@gmail.com>
2024-09-13 10:35:34 -0300

    função de logout

--- Hora de Codar/09 - Projeto MiniBlog/projeto/src/hooks/useAuthentication.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/projeto/src/hooks/useAuthentication.jsx
@@ -1,90 +1,86 @@
-import {db} from "../firebase/config"
+import { db } from "../firebase/config";
 
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    updateProfile,
-    signOut
-} from 'firebase/auth'
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
- const [error,setError]=useState(null);
- const [loading,setLoading]=useState(null);
+  //cleanup
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
 
- //cleanup
- //deal with memory leak
- const[cancelled, setCancelled]=useState(false);
+  const auth = getAuth();
 
- const auth = getAuth();
-
-
- function checkIfCancelled(){
-    if(cancelled){
-        return;
+  function checkIfCancelled() {
+    if (cancelled) {
+      return;
     }
- }
+  }
 
- const createUser = async (data) => {
-    
+  //register
+  const createUser = async (data) => {
     checkIfCancelled();
 
     setLoading(true);
     setError(null);
 
     try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
 
-        const { user } = await createUserWithEmailAndPassword(
-            auth,
-            data.email,
-            data.password
-        )
+      await updateProfile(user, {
+        displayName: data.datadisplayName,
+      });
 
-        await updateProfile(user, {
+      setLoading(false);
 
-            displayName: data.datadisplayName,
-        });
-
-        setLoading(false);
-
-        return user
-        
+      return user;
     } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
 
-        console.log(error.message);
-        console.log(typeof error.message);
+      let systemErrorMessage;
 
-        let systemErrorMessage
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "Senha fraca!";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "Email já cadastrado!";
+      } else {
+        systemErrorMessage = "Houve um erro,  tenta mais tarde!";
+      }
 
-        if(error.message.includes("Password")){
-            systemErrorMessage = "Senha fraca!"
-        }else if(error.message.includes("email-already")){
-            systemErrorMessage = "Email já cadastrado!"
-        }else{
-            systemErrorMessage = "Houve um erro,  tenta mais tarde!"
-        }
-
-        setLoading(false);
-        setError(systemErrorMessage);
+      setLoading(false);
+      setError(systemErrorMessage);
     }
+  };
 
-   
- }
+  //logout
+  const logout = () => {
+    checkIfCancelled();
+    signOut(auth);
+  };
 
- useEffect(()=>{
-    return ()=> setCancelled(true);
- },[]);
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
 
- return {
+  return {
     auth,
     createUser,
     error,
     loading,
-    
- };
-
+    logout,
+  };
 };
-
