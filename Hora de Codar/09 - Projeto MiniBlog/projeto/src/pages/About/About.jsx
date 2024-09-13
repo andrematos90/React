f4186d7ca16192ce7365be9063f9c5f69b6caad1
André Matos <andrerosadematos90@gmail.com>
2024-09-13 10:21:37 -0300

    bloqueando páginas que precisam de autenticação

--- Hora de Codar/09 - Projeto MiniBlog/projeto/src/pages/About/About.jsx
+++ Hora de Codar/09 - Projeto MiniBlog/projeto/src/pages/About/About.jsx
@@ -1,9 +1,17 @@
 //css
 import styles from './../About/About.module.css'
 
+//router
+import { Link } from 'react-router-dom'
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.about}>
+      <h2>Sobre o <span>MiniBlog</span></h2>
+      <p>Este projeto de estudo usa React no front-end e firebase do backend</p>
+
+      <Link to='posts/create' className="btn">Criar Post</Link>
+    </div>
   )
 }
 
